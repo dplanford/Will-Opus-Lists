25caecc54641a870247a30f72e7f0fc7bdf3677f
Doug Lanford <opus@opusgames.com>
2026-09-06 14:12:10 -0600

    (WIP) Added a helper function for grabbing the user's master list.

--- lib/helper/willopus_master_list_helper.dart
+++ lib/helper/willopus_master_list_helper.dart
@@ -1,1 +1,30 @@
 //import 'package:willopuslists/model/willopus_master_list.dart';
+import 'package:willopuslists/model/willopus_master_list.dart';
+import 'package:willopuslists/services/willopus_master_services.dart';
+
+class WillOpusMasterListHelper {
+  Future<WillOpusMasterList?> getMaster() async {
+    String? masterId = await WillOpusMasterServices.getMasterKey();
+    if (masterId == null) {
+      // No master key stored yet... create a new master list object.
+      var masterList = WillOpusMasterList();
+      masterId = await WillOpusMasterServices.addMasterList(masterList);
+      if (masterId != null) {
+        WillOpusMasterServices.setMasterKey(masterId);
+        return masterList;
+      }
+      return null;
+    }
+
+    WillOpusMasterList? masterList = await WillOpusMasterServices.getMasterList(masterId);
+    if (masterList == null) {
+      // Error... we have a stored key, but no associated object.
+      // TODO: Create a new master list, thus starting from scratch?
+      // Try to recover by searching for a stored master list object with a malformed key?
+      // There should never be more than one master list object stored per user....
+      return null;
+    }
+
+    return masterList;
+  }
+}
