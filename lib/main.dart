4aea86e279357e481ab0767aca7dda327277a3f6
Doug Lanford <opus@opusgames.com>
2026-09-08 21:10:10 -0600

    (WIP) A couple more comment adjustments & modifications to my TODO text file....

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,11 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    // TODO: Check for initial binding before doing anything?
+    //  - that initial wait at app start before doing anything?
+
     // TODO: Fix shared prefs!!!!!
+    // I think fixed with first access? Make sure any try async waits!?
     //WidgetsBinding.instance.addPostFrameCallback((_) async {
     //  kSharedPreferences = await SharedPreferences.getInstance();
     //});
