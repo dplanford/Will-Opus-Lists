47db4e3c80f3f9724af5197f2ca7390135b88bdc
Doug Lanford <opus@opusgames.com>
2026-09-10 20:23:24 -0600

    (WIP) Evening thoughts about a list tile for the master screen, for each stored list. Absolutely not tested yet, or even hooked up anything yet....

--- lib/widgets/willopus_list_tile.dart
+++ lib/widgets/willopus_list_tile.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:willopuslists/helper/willopus_color_helper.dart';
 
-import 'package:willopuslists/model/willopus_master_list.dart';
+import 'package:willopuslists/model/willopus_list.dart';
 
 class WillOpusListTile extends StatefulWidget {
-  final WillOpusMasterList masterList;
+  final WillOpusList list;
   final void Function()? refreshParent;
 
-  const WillOpusListTile({super.key, required this.masterList, this.refreshParent});
+  const WillOpusListTile({super.key, required this.list, this.refreshParent});
 
   @override
   State<WillOpusListTile> createState() => _WillOpusListTileState();
@@ -28,6 +29,61 @@ class _WillOpusListTileState extends State<WillOpusListTile> {
     // Whole tile is wrapped in a tap gesture (with tile icons overlayed/overridding the general tap)
     //  - general tap on list tile goes to that list's display screen....
     //
-    return Container();
+    //return Container();
+    double tableWidth = MediaQuery.of(context).size.width - 24;
+    return Container(
+      width: tableWidth,
+      //color: widget.item.isCompleted ? Theme.of(context).primaryColor : Colors.white,
+      margin: const EdgeInsets.all(12.0),
+      // TODO: wrap in a tap gesture widget for going to the list display details screen....
+      // This must respect the overlay of tile buttons (edit/delete) over the general tap behavior.
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 32.0,
+                  child: Container(color: WillOpusColorHelper.colorFromHex(widget.list.hexColor)),
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  widget.list.title,
+                  //style: TextStyle(fontSize: 20.0, color: widget.list.isCompleted ? Colors.white : Colors.black),
+                  softWrap: true,
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  widget.list.desc,
+                  //style: TextStyle(fontSize: 12.0, color: widget.item.isCompleted ? Colors.white : Colors.black),
+                  softWrap: true,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 8.0),
+          Column(
+            children: [
+              IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () async {
+                  // TODO: popup to edit list dialog, and save any edits....
+                  // Should be same as add new list dialog, with slightly different hooks....
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () async {
+                  // TODO: popup "are you sure" dialog before deleting this list and all it's sub-items?
+                  // Need new delete entire list helper func?
+                  //  - delete all sub-items, then delete main list?
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
