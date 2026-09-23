f1ffa709ece61f796578156b3b6cc2c50e5ce388
Doug Lanford <opus@opusgames.com>
2026-09-23 12:20:56 -0600

    (WIP) Left myself a note about localization.

--- lib/constants.dart
+++ lib/constants.dart
@@ -10,3 +10,8 @@ const String kTestFile = 'TestList/test';
 //const String kTestFileItem = 'TestList/test/@ID.json';
 
 const String kMasterIDKey = 'master_id';
+
+// TODO: NOTE: This uses auto-generated localizations that in turn become strings 
+// embedded in the app. For smaller apps with only a few translated languages, this 
+// is fine, and very fast. If later we have more languages and/or more translated 
+// strings, we may move to loading each language seperately as it's own file.
